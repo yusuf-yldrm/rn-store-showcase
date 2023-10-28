abeda6a73781729eed0120c8934e4176ecb67e76
Yusuf Yıldırım <dev.yusufyildirim@gmail.com>
2023-10-28 18:00:30 +0300

    Local Redux Test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,31 @@
-import { StyleSheet } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 
+import { useState } from "react";
 import { InterBoldText } from "../../src/components/StyledText";
 import { View } from "../../src/components/Themed";
-
+import { useAppDispatch, useAppSelector } from "../../src/store/hooks";
+import {
+  incrementAsync,
+  selectCount,
+} from "../../src/store/slices/counterSlice";
 export default function DiscoverScreen() {
+  const [incrementAmount, setIncrementAmount] = useState("2");
+
+  // The `state` arg is correctly typed as `RootState` already
+  const count = useAppSelector(selectCount);
+  const status = useAppSelector((state) => state.counter.status);
+  const dispatch = useAppDispatch();
+
   return (
     <View style={styles.container}>
-      <InterBoldText>Test</InterBoldText>
+      <InterBoldText>{count}</InterBoldText>
+      <Pressable
+        onPress={() => {
+          dispatch(incrementAsync(10));
+        }}
+      >
+        <InterBoldText>{status}</InterBoldText>
+      </Pressable>
     </View>
   );
 }
